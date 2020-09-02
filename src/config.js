d8004adc48dc5e09e32655cbf3899fa435626920
JTLiner925 <[jtliner925@gmail.com]>
2020-09-02 12:26:29 -0500

    working on pushing to heroku with variables

--- src/config.js
+++ src/config.js
@@ -4,10 +4,11 @@ dotenv.config();
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL:
-    process.env.DB_URL || 'postgresql://postgres@localhost/prayer-circles',
-  TEST_DB_URL:
-    process.env.TEST_DB_URL ||
+  DATABASE_URL:
+    process.env.DATABASE_URL ||
+    'postgresql://postgres@localhost/prayer-circles',
+  TEST_DATABASE_URL:
+    process.env.TEST_DATABASE_URL ||
     'postgresql://postgres@localhost/prayer-circles-test',
   region: process.env.REGION,
   accessKeyId: process.env.accessKeyId,
